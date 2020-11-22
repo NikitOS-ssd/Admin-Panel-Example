65d721e41a7b1ec13b8206a3705464798f448a4d
NikitOs <exemplum777@gmail.com>
2020-11-22 14:26:15 +0300

    init

---
+++ src/authServices/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+export const config = {
+  apiKey: "AIzaSyBPvB9c5gLrCtf81ozfCnGkwEFPxK_7ukU",
+  authDomain: "ihtys-admin.firebaseapp.com",
+  databaseURL: "https://ihtys-admin.firebaseio.com",
+  projectId: "ihtys-admin",
+  storageBucket: "ihtys-admin.appspot.com",
+  messagingSenderId: "871228421488",
+  appId: "1:871228421488:web:c7e535048d65b5e7488bb5",
+  measurementId: "G-RY08W9B1J3"
+}
